6d0c9787527bc1720dc509527a42d77c1526dadc
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-05 18:18:36 -0400

    Create API endpoint for challenges index

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :challenges, only: [:index]
+
+  namespace :api do
+    namespace :v1 do
+      resources :challenges, only: [:index]
+    end
+  end
 end
