a2e36faa97b042260219d99f8baf0d019a53eae8
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-06 18:15:38 -0400

    Create challenge factory

---
+++ spec/factories/challenge.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :challenge do
+    user
+    sequence(:min_rank) do
+      range = (-30..-1).to_a.concat((1..9).to_a).sample
+    end
+    sequence(:max_rank) do
+      range = []
+      if min_rank > 0
+        range = min_rank..9.to_a
+      else
+        range = (min_rank..-1).to_a.concat((1..9).to_a)
+      end
+      range.sample
+    end
+  end
+end
