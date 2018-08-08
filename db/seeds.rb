3ad32ac9baf048580781c4350fa5d6f37c571cdb
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-08 15:55:29 -0400

    Create Move seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,38 @@ Challenge.create!(
 )
 
 FactoryBot.create_list(:challenge, 20)
+
+Game.create()
+
+Move.create(
+  move_number: 1,
+  color: 'black',
+  is_pass: false,
+  x: 4,
+  y: 4,
+  game: Game.last
+)
+Move.create(
+  move_number: 2,
+  color: 'white',
+  is_pass: false,
+  x: 4,
+  y: 5,
+  game: Game.last
+)
+Move.create(
+  move_number: 3,
+  color: 'black',
+  is_pass: false,
+  x: 5,
+  y: 4,
+  game: Game.last
+)
+Move.create(
+  move_number: 4,
+  color: 'white',
+  is_pass: false,
+  x: 3,
+  y: 4,
+  game: Game.last
+)
