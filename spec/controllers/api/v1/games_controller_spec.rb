214c546be64a8f02dd4e0a3c159c00582806d048
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-08 17:08:46 -0400

    Create test for receiving game moves from API endpoint

--- spec/controllers/api/v1/games_controller_spec.rb
+++ spec/controllers/api/v1/games_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::GamesController, type: :controller do
   let!(:game_1) { FactoryBot.create(:game) }
   let!(:move_1) { FactoryBot.create(:move, game: game_1, move_number: 1) }
+  let!(:move_2) { FactoryBot.create(:move, game: game_1, move_number: 2) }
 
   describe 'GET#show' do
     it 'should return the correct game' do
@@ -16,6 +17,19 @@ RSpec.describe Api::V1::GamesController, type: :controller do
       expect(response.content_type).to eq('application/json')
 
       expect(returned_json['id']).to eq(game_1.id)
+    end
+
+    it 'should return moves associated with the game' do
+      get :show, params: {
+        id: game_1.id,
+        game: { id: game_1.id }
+      }
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq('application/json')
+
+      expect(returned_json[:moves].length).to eq(game_1.moves.length)
     end
   end
 end
