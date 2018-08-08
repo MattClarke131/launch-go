886edee9c3bdd4fa8d659e31d69beef9cf4f0e94
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-08 14:11:00 -0400

    Create games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_195320) do
+ActiveRecord::Schema.define(version: 2018_08_08_172552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2018_08_05_195320) do
     t.integer "max_rank", default: 9
     t.bigint "user_id"
     t.index ["user_id"], name: "index_challenges_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
