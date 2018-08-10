8bbb2128aa5efba4c0fd41c5ef4ef998c9ddf43d
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-10 11:13:05 -0400

    Include board_states in GameSerializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -3,4 +3,12 @@ class GameSerializer < ActiveModel::Serializer
 
   has_many :moves
   has_many :board_states
+
+  def board_states
+    result = object.board_states.order('move_number DESC')
+    result.map do |board_state|
+      new_state = board_state
+      new_state['board'] = JSON.parse(new_state['board'])
+    end
+  end
 end
