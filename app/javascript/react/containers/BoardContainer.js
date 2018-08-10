92f6a2f6b63015298173222e6a0d50dd53a49176
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-10 13:16:53 -0400

    create createGameBoard method

--- app/javascript/react/containers/BoardContainer.js
+++ app/javascript/react/containers/BoardContainer.js
@@ -1,16 +1,40 @@
 import React from 'react'
+import BoardTile from '../components/BoardTile'
 
 class BoardContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
+    this.createGameBoard = this.createGameBoard.bind(this)
+  }
+
+  createGameBoard() {
+    let board = this.props.boardState.map((row, y) => {
+      let populatedRow = row.map((item, x) => {
+        return (
+          <BoardTile
+            key={`x:${x+1},y:${y+1}`}
+            x={x+1}
+            y={y+1}
+            color={item}
+          />
+        )
+      })
+
+      return (
+        <div key={`row: ${y}`} className='gameRow'>
+          {populatedRow}
+        </div>
+      )
+    })
+
+    return board
   }
 
   render() {
-    return(
+    let gameBoard = this.createGameBoard()
+    return (
       <div className='board-container'>
-
+        {gameBoard}
       </div>
     )
   }
