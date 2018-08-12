f98221b08caf6ec5de5bd0e413d536d19b3eb39d
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-11 22:45:03 -0400

    Change NewChallengeFrom from const to class

--- app/javascript/react/components/NewChallengeForm.js
+++ app/javascript/react/components/NewChallengeForm.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import SelectTile from './SelectTile'
 
-const NewChallengeForm = (props) => {
-  return (
-    <form className='new-challenge-form'>
-      <SelectTile />
-    </form>
-  )
+class NewChallengeForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <form className='new-challenge-form'>
+        <SelectTile />
+      </form>
+    )
+  }
 }
 
 export default NewChallengeForm;
