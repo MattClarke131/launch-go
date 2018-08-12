fd19e44f85540dde05c347cba31fe01c1e63272f
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-11 23:09:15 -0400

    Create controlled select fields in challenge form

--- app/javascript/react/components/NewChallengeForm.js
+++ app/javascript/react/components/NewChallengeForm.js
@@ -4,11 +4,38 @@ import SelectTile from './SelectTile'
 class NewChallengeForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      minRank: '',
+      maxRank: '',
+    }
+    this.handleMinChange = this.handleMinChange.bind(this)
+    this.handleMaxChange = this.handleMaxChange.bind(this)
   }
+
+  handleMinChange(event) {
+    this.setState({
+      minRank: event.target.value
+    })
+  }
+  handleMaxChange(event) {
+    this.setState({
+      maxRank: event.target.value
+    })
+  }
+
   render() {
     return (
       <form className='new-challenge-form'>
-        <SelectTile />
+        <SelectTile
+          value={this.state.minRank}
+          label={'Min. Rank'}
+          handleChange={this.handleMinChange}
+        />
+        <SelectTile
+          value={this.state.maxRank}
+          label={'Max. Rank'}
+          handleChange={this.handleMaxChange}
+        />
       </form>
     )
   }
--- app/javascript/react/components/SelectTile.js
+++ app/javascript/react/components/SelectTile.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const SelectTile = (props) => {
-  return(
-    <select>
+  let options = [
+    <option key={'emptyField'} />
+  ]
+  for(let i = -30; i <= 9; i++) {
+    if(i < 0) {
+      options.push(
+        <option key={i} value={i}>{`${i}k`}</option>
+      )
+    } else if (i > 0) {
+      options.push(
+        <option key={i} value={i}>{`${i}d`}</option>
+      )
+    }
+  }
 
-    </select>
+  return(
+    <label>
+      {props.label}
+      <select value={props.value} onChange={props.handleChange}>
+        {options}
+      </select>
+    </label>
   )
 }
 
