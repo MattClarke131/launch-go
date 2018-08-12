5b400c753eb7b6770a1cc4fec4b46211d9280146
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-12 17:23:52 -0400

    Remove unnecessary seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,43 +3,7 @@ User.create!(
   password: "222222",
   is_admin: true
 )
-Challenge.create!(
-  user_id: 1
-)
 
 FactoryBot.create_list(:challenge, 20)
 
 FactoryBot.create_list(:game, 20, size: 9)
-
-Move.create!(
-  move_number: 1,
-  color: 'black',
-  is_pass: false,
-  x: 4,
-  y: 4,
-  game: Game.last
-)
-Move.create!(
-  move_number: 2,
-  color: 'white',
-  is_pass: false,
-  x: 4,
-  y: 5,
-  game: Game.last
-)
-Move.create!(
-  move_number: 3,
-  color: 'black',
-  is_pass: false,
-  x: 5,
-  y: 4,
-  game: Game.last
-)
-Move.create!(
-  move_number: 4,
-  color: 'white',
-  is_pass: false,
-  x: 3,
-  y: 4,
-  game: Game.last
-)
