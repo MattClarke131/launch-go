c59352fec230124ab8ba7da48b9d703b152f2cf5
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-11 21:59:02 -0400

    Style game into tile

--- app/javascript/react/components/GameInfoTile.js
+++ app/javascript/react/components/GameInfoTile.js
@@ -3,12 +3,9 @@ import React from 'react';
 const GameInfoTile = (props) => {
   return(
     <div className='game-info-tile'>
-      <div className='player_black'>
-        {props.player_black.email}
-      </div>
-      <div className='player_white'>
-        {props.player_white.email}
-      </div>
+        BLACK: {props.player_black.email}
+        <br/>
+        WHITE: {props.player_white.email}
     </div>
   )
 }
