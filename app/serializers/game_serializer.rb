4d00a336b03238158ac6c56ef56832732c030cfb
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-11 21:00:59 -0400

    Add player data to game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -3,6 +3,8 @@ class GameSerializer < ActiveModel::Serializer
 
   has_many :moves
   has_many :board_states
+  has_many :players
+
 
   def board_states
     result = object.board_states.order('move_number DESC')
@@ -10,5 +12,17 @@ class GameSerializer < ActiveModel::Serializer
       new_state = board_state
       new_state['board'] = JSON.parse(new_state['board'])
     end
+  end
+
+  def players
+    players = object.pairings.map do |pairing|
+      {
+        email: pairing.user.email,
+        rank: pairing.user.rank,
+        color: pairing.color
+      }
+    end
+
+    players
   end
 end
