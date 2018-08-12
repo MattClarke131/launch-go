be47339fe223799270691af45811e6bd2f8bbd55
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-12 18:24:05 -0400

    Create endpoint for new challenge post

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ChallengesController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
     @challenges = Challenge.all
     render json: @challenges, each_serializer: ChallengeSerializer
+  end
+
+  def create
   end
 end
