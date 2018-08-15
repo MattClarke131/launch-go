416899664525341d4a62b2532b275e6c74dbbc20
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-14 21:03:33 -0400

    pass challenge.id to addNewChallenge in ChallengesContainer

--- app/serializers/challenge_serializer.rb
+++ app/serializers/challenge_serializer.rb
@@ -1,5 +1,5 @@
 class ChallengeSerializer < ActiveModel::Serializer
-  attributes :max_rank, :min_rank, :user, :current_user
+  attributes :id, :max_rank, :min_rank, :user, :current_user
 
   has_one :user
 
