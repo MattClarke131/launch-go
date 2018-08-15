b73774e495eecba58a3720dba7c3b66dea115651
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-14 21:12:31 -0400

    Add api/v1/challenges show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :challenges, only: [:index, :create, :destroy]
+      resources :challenges, only: [:index, :create, :destroy, :show]
       resources :games, only: [:show, :create]
     end
   end
