ac42b990cb9587dcab4f13c93b3b7bebeb0fc585
Matthew Clarke <MattClarkeRuns@gmail.com>
2018-08-16 19:12:27 -0400

    Refactor makeMove data type

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -24,14 +24,17 @@ class Api::V1::GamesController < ApplicationController
 
   def update
     game = Game.find(params[:id])
-    board_states = game.board_states.order('move_number DESC')
-    newest_state = board_states[0]
 
-    if(
-      authorize_move(game, params[:x], params[:y]) &&
-      authorize_player(game)
-    )
-      update_game(game, params[:x], params[:y])
+    if authorize_player(game)
+      if params[:type] === 'move' && authorize_move(game, params[:x], params[:y])
+
+        move = {
+          type: 'move',
+          x: params[:x],
+          y: params[:y]
+        }
+        update_game(game, move)
+      end
     end
 
     render json: game
@@ -65,14 +68,19 @@ class Api::V1::GamesController < ApplicationController
     active_player_id === User.current_user.id
   end
 
-  def update_game(game, x, y)
+  def update_game(game, move)
     board_states = game.board_states.order('move_number DESC')
     newest_state = board_states[0]
     new_board = JSON.parse(newest_state.board)
     move_color = newest_state.move_number % 2 === 0 ? 'black' : 'white'
-    new_board[x][y] = move_color
+    if move[:type] === 'move'
+      x = move[:x]
+      y = move[:y]
+      new_board[x][y] = move_color
+    elsif move[:type] === 'pass'
+      # do nothing
+    end
     new_board = JSON.generate(new_board)
-
     new_state = BoardState.new(
       game: game,
       move_number: newest_state.move_number + 1,
--- app/javascript/react/containers/GameShowContainer.js
+++ app/javascript/react/containers/GameShowContainer.js
@@ -49,10 +49,13 @@ class GameShowContainer extends React.Component {
     return boardState
   }
 
-  makeMove(x_coord,y_coord) {
+  makeMove(moveObject) {
     let formPayload = {
-      x: x_coord-1,
-      y: this.state.game.board_states[0][0].length-y_coord
+      type: moveObject.type
+    }
+    if (moveObject.type === 'move') {
+      formPayload['x'] = moveObject.x-1
+      formPayload['y'] = this.state.game.board_states[0][0].length-moveObject.y
     }
     fetch(`/api/v1/games/${this.state.game.id}`, {
       credentials: 'same-origin',
